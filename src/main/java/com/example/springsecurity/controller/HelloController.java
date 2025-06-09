66a25e458cfddb95f654bde1e157e1da01d1a582
KHADGA-R <parajuli.kr@gmail.com>
2025-06-08 18:28:19 -0700

    setting RestController class

---
+++ src/main/java/com/example/springsecurity/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.springsecurity.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/welcome")
+    public String sayHello(){
+        return "Welcome to Spring Security w/o  jwt and Oauth2 implementation .";
+    }
+}
