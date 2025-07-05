c14473a30dcddcb7bea3ffd491e5bf9f05b4fcfb
KHADGA-R <parajuli.kr@gmail.com>
2025-07-04 22:10:29 -0700

    adding sql script to create new custom table for authentication

--- src/main/resources/sql/scripts.sql
+++ src/main/resources/sql/scripts.sql
@@ -9,3 +9,21 @@ INSERT INTO `authorities` VALUES ('user', 'read');
 
 INSERT IGNORE INTO `users` VALUES ('admin', '{bcrypt}$2a$12$Gr7Q.ZmM9Twzwu.UJokz4uwULTaRejhLsR64uvgg79821Gx8ReZue', '1');
 INSERT INTO `authorities` VALUES ('admin', 'admin');
+
+
+UPDATE authorities SET authority = 'ROLE_USER' WHERE username = 'admin';
+UPDATE users
+SET password = '{bcrypt}$2a$10$lwECMIp/YSO.Ews8OYsJi.Tmkih20xTIJEp8jP2yw3TPzGIIibzWW'
+WHERE username = 'admin';
+
+
+CREATE TABLE `customer` (
+`id` int NOT NULL AUTO_INCREMENT,
+`email` varchar(45) NOT NULL,
+`pwd` varchar(200) NOT NULL,
+`role` varchar(45) NOT NULL,
+PRIMARY KEY(`id`)
+);
+
+INSERT INTO `customer` (`email`, `pwd`, `role`) VALUES ('user@example.com', '{noop}MeHero@12345', 'read');
+INSERT INTO `customer` (`email`, `pwd`, `role`) VALUES ('admin@example.com', '{bcrypt}$2a$10$lwECMIp/YSO.Ews8OYsJi.Tmkih20xTIJEp8jP2yw3TPzGIIibzWW', 'admin');
